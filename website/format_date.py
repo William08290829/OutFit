5617de0bfee893508cad071f264862b6caa14fd9
William Chen <a0935109965@gmail.com>
2024-03-30 01:17:36 +0800

    Update format_date.py to include Chinese translations for weekdays

--- website/format_date.py
+++ website/format_date.py
@@ -1,17 +1,30 @@
-import locale
+# import locale
 from datetime import datetime, timedelta
+
+weekday_translation = {
+    "Monday": "星期一",
+    "Tuesday": "星期二",
+    "Wednesday": "星期三",
+    "Thursday": "星期四",
+    "Friday": "星期五",
+    "Saturday": "星期六",
+    "Sunday": "星期日"
+}
+
 
 def format_date(input_date):
     # 設置地區為中文
-    locale.setlocale(locale.LC_TIME, 'zh_TW.UTF-8')
+    # locale.setlocale(locale.LC_TIME, 'zh_TW.UTF-8')
     
     # 將字符串轉換為日期對象
     
     # 格式化日期
     formatted_date = input_date.strftime("%#m/%d")
-    weekday =  input_date.strftime("%A")
+    weekday_english =  input_date.strftime("%A")
     
-    return formatted_date, weekday
+    weekday_chinese = weekday_translation[weekday_english]
+    
+    return formatted_date, weekday_chinese
 
 def get_future_date(days_offset):
     today = datetime.now().date()
